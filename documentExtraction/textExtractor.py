686b49cb0b301234a311ee2c3aa2110ef8beb293
KungfuNaman <naman@namans-air.home>
2024-06-12 14:19:44 +0100

    added extract text with images with feature

---
+++ documentExtraction/textExtractor.py
@@ -0,0 +1,28 @@
+from langchain_community.document_loaders import PyMuPDFLoader
+import pymupdf4llm
+import os
+
+
+def extractText(pdfPath):
+    loader = PyMuPDFLoader(pdfPath)
+    data = loader.load()
+    with open("./extractText", "w", encoding="utf-8") as file:
+        for page in data:
+            file.write(page.page_content)
+            file.write("\n\n")
+
+
+def extractWithImages(pdfPath):
+
+    md_text = pymupdf4llm.to_markdown(pdfPath, write_images=True)
+
+    output_dir = "./documentExtraction/outputs/extractWithImages.txt"
+
+    with open(output_dir, "w", encoding="utf-8") as file:
+        # Write the markdown text to the file
+        file.write(md_text)
+        file.write("\n\n")
+
+
+# extractTextUsingLangChain("./documents/2.pdf")
+extractWithImages("./documents/3.pdf")
