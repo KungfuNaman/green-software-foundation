35f787ce01dc5c6f2e60f846423c7c0b35bcad44
KungfuNaman <naman@eduroam-int-dhcp-97-29-249.ucl.ac.uk>
2024-06-13 18:44:35 +0100

    added loggers

--- Rag/query_data.py
+++ Rag/query_data.py
@@ -31,17 +31,19 @@ def query_rag(query_text: str):
     # Prepare the DB.
     embedding_function = get_embedding_function()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
-
+    print("data added to db")
     # Search the DB.
     results = db.similarity_search_with_score(query_text, k=5)
+    print("context is taken out")
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    # print(prompt)
+    print("prompt is created")
 
     model = Ollama(model="llama2")
     response_text = model.invoke(prompt)
+    print("response is generated")
 
     sources = [doc.metadata.get("id", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
