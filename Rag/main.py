cc2df6e00db4663f76493be90c103fadc1831d9e
KungfuNaman <39888498+KungfuNaman@users.noreply.github.com>
2024-06-18 00:18:51 +0100

    reverted the changes in the main branch to prevent the code from breaking

--- Rag/main.py
+++ Rag/main.py
@@ -9,7 +9,7 @@ def main():
     query_text = "can you tell me which and how many servers are used   ?"
     setup_database_start_time = time.time()
 
-    is_document_embedded = setup_database(document_path, False)
+    is_document_embedded=setup_database(document_path, False)
     setup_database_end_time = time.time()
 
     setup_database_time = setup_database_end_time - setup_database_start_time if is_document_embedded else "0"
--- Rag/query_data.py
+++ Rag/query_data.py
@@ -3,14 +3,10 @@ from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 import time
-from hf_inference_model import Embedder, Extractor
+from get_embedding_function import get_embedding_function
 from logger.get_track_llm_response import append_to_csv
-import chromadb
-
 
 CHROMA_PATH = os.getenv("CHROMA_PATH")
-HF_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
-MODEL_ID = ''
 
 PROMPT_TEMPLATE = """
 Answer the question based only on the following context:
@@ -24,26 +20,22 @@ Answer the question based on the above context: {question}
 
 
 def main():
-    query_rag("can you tell me the databases details getting used ?", "")
+    query_rag("can you tell me the databases details getting used ?","")
 
 
-def query_rag(query_text: str, setup_database_time: str):
+def query_rag(query_text: str,setup_database_time: str):
     # Prepare the DB.
-    embedder = Embedder()
-    persistent_client = chromadb.PersistentClient()
-    collection = persistent_client.get_or_create_collection("dim384")
-    db = Chroma(
-        persist_directory=CHROMA_PATH,
-        client=persistent_client,
-        collection_name="dim384",
-        embedding_function=embedder,
-    )
+    embedding_function = get_embedding_function()
+
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
     print("data added to db : ", setup_database_time, "s")
 
     # Search the DB.
+
     search_start_time = time.time()
 
-    similarity_results = db.similarity_search_with_score(query_text, k=5)  # [(Document(), sort_of_sim_rate)]
+    similarity_results = db.similarity_search_with_score(query_text, k=5)
 
     search_end_time = time.time()
     search_time = search_end_time - search_start_time
@@ -58,23 +50,17 @@ def query_rag(query_text: str, setup_database_time: str):
 
     response_start_time = time.time()
 
-    extractor = Extractor()
-    response = extractor.generate_answer(prompt)
-    response_text = response[0]['generated_text']
-
-    print('ANSWER---')
-    print(response_text)
-    print('ANSWER---')
+    model = Ollama(model="llama2")
+    response_text = model.invoke(prompt)
 
     response_end_time = time.time()
     response_time = response_end_time - response_start_time
 
-    print("response is generated: ", response_time, "s")
+    print("response is generated : ", response_time, "s")
 
     sources = [doc.metadata.get("id", None) for doc, _score in similarity_results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
-
-    # print(formatted_response)
+    print(formatted_response)
 
     append_to_csv(
         query_text, context_text, search_time, response_text, response_time, setup_database_time,similarity_results
