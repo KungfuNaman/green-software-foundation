29872b735e2540d38f356c4cf7312e0f3e4efbcd
KungfuNaman <39888498+KungfuNaman@users.noreply.github.com>
2024-06-20 18:24:54 +0100

    added backend folder,added checkstyle for google coding style checks

---
+++ backend/src/test/java/com/example/ecoDocSense/EcoDocSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecoDocSense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoDocSenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
