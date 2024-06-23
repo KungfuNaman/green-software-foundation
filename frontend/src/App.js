5d04c9d1b45e3249f9e23a85ae3cadb8598a6de1
KungfuNaman <39888498+KungfuNaman@users.noreply.github.com>
2024-06-23 18:42:02 +0100

    Feature frontend template (#37)
    * added template code for react frontned
    
    * updated the read me
    
    * updated readme and gitignore file
    
    * updated git ignore

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Eco Doc Sense
+      </header>
+    </div>
+  );
+}
+
+export default App;
