57dc600088c92241636d12ac2135dd0e7c62d7e2
KungfuNaman <39888498+KungfuNaman@users.noreply.github.com>
2024-06-30 00:42:03 +0100

    parser code

--- Rag/parser.py
+++ Rag/parser.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-CSV_FILE_PATH="/Users/naman/Documents/groupProject/green-software-foundation/Rag/logger/test.csv"
+CSV_FILE_PATH="/Users/naman/Documents/groupProject/green-software-foundation/Rag/logger/parsedResults.csv"
 def read_ecoDoc_results(csv_file_path):
     # Read the CSV file into a DataFrame
     df = pd.read_csv(csv_file_path)
@@ -11,24 +11,43 @@ def read_ecoDoc_results(csv_file_path):
 
 def add_parsed_results(csv_file_path):
     records=read_ecoDoc_results(csv_file_path)
-    final_answer_arr=[]
+    conclusion_arr=[]
     explanation_arr=[]
     result_arr=[]
     for item in records:
         generated_response=item["response_text"]
         explanation,final_answer,result=parse_generated_response(generated_response)
         explanation_arr.append(explanation)
-        final_answer_arr.append(final_answer)
+        conclusion_arr.append(final_answer)
         result_arr.append(result)
     print("hello")
-    # updated_records = pd.DataFrame(records)
-    # updated_records.to_csv('path/to/your/updated_records.csv', index=False)
+     # Add the new columns to the DataFrame
+    df = pd.DataFrame(records)
+    
+    # Add the new columns to the DataFrame
+    df['explanation'] = explanation_arr
+    df['conclusion'] = conclusion_arr
+    df['result'] = result_arr
+
+    # Save the updated DataFrame to a CSV file
+    df.to_csv('/Users/naman/Documents/groupProject/green-software-foundation/Rag/logger/COMBINED_RESULTS.csv', index=False)
 
 def parse_generated_response(generated_response):
      # Extract the explanation
-    response_start = generated_response.find("Response:") + len("Response:")
-    response_end = generated_response.find("Conclusion:")
-    response = generated_response[response_start:response_end].strip()
+    response_start = None
+    if "Response:" in generated_response:
+        response_start = generated_response.find("Response:") + len("Response:")
+    elif "Answer:" in generated_response:
+        response_start = generated_response.find("Answer:") + len("Answer:")
+    
+    if response_start is not None:
+        response_end = generated_response.find("Conclusion:")
+        if response_end == -1:  # if "Conclusion:" is not found
+            response = generated_response[response_start:].strip()
+        else:
+            response = generated_response[response_start:response_end].strip()
+    else:
+        response = ""
     
     # Extract the conclusion
     conclusion_start = generated_response.find("Conclusion:") + len("Conclusion:")
