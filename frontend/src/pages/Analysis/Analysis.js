e9d2b3df21b16f6883706d286086d991520782f9
KungfuNaman <39888498+KungfuNaman@users.noreply.github.com>
2024-07-01 17:06:57 +0100

    added bar chart

--- frontend/src/pages/Analysis/Analysis.js
+++ frontend/src/pages/Analysis/Analysis.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Analysis.css';
 import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
+import { PieChart } from '@mui/x-charts/PieChart';
+import { BarChart } from '@mui/x-charts/BarChart';
 
 const settings = {
   width: 200,
   height: 200,
   value: 60,
 };
+const data = [
+  { id: 0, value: 10, label: 'series A' },
+  { id: 1, value: 15, label: 'series B' },
+  { id: 2, value: 20, label: 'series C' },
+];
 
 const Home = () => {
   return (
@@ -25,6 +32,22 @@ const Home = () => {
           fill: theme.palette.text.disabled,
         },
       })}
+    />
+     <PieChart
+      series={[
+        {
+          data,
+          highlightScope: { faded: 'global', highlighted: 'item' },
+          faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+        },
+      ]}
+      height={200}
+    />
+    <BarChart
+      xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+      series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+      width={500}
+      height={300}
     />
     </>
   );
