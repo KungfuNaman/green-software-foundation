2fc6c59c457bcccdb3b837d0b7929b79f6ea5a42
KungfuNaman <39888498+KungfuNaman@users.noreply.github.com>
2024-07-01 16:55:16 +0100

    added gauge chart to see the progress

--- frontend/src/pages/Analysis/Analysis.js
+++ frontend/src/pages/Analysis/Analysis.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './Analysis.css';
+import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
 
+const settings = {
+  width: 200,
+  height: 200,
+  value: 60,
+};
 
 const Home = () => {
   return (
     <>
-     
+      <Gauge
+      {...settings}
+      cornerRadius="50%"
+      sx={(theme) => ({
+        [`& .${gaugeClasses.valueText}`]: {
+          fontSize: 40,
+        },
+        [`& .${gaugeClasses.valueArc}`]: {
+          fill: '#52b202',
+        },
+        [`& .${gaugeClasses.referenceArc}`]: {
+          fill: theme.palette.text.disabled,
+        },
+      })}
+    />
     </>
   );
 };
