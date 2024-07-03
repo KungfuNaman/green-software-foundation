97cc5cab58dbe4fd7b1c418aa686085b91e0a9f2
KungfuNaman <39888498+KungfuNaman@users.noreply.github.com>
2024-07-03 03:30:43 +0100

    completed bar chart

--- frontend/src/components/ResultBarChart/ResultBarChart.js
+++ frontend/src/components/ResultBarChart/ResultBarChart.js
@@ -1,18 +1,53 @@
-import * as React from 'react';
-import { BarChart } from '@mui/x-charts/BarChart';
+import React, { useEffect, useState } from "react";
 
+import { BarChart } from "@mui/x-charts/BarChart";
+import { axisClasses } from "@mui/x-charts/ChartsAxis";
 
-
-export default function ResultBarChart({xlabels,yesDataArr,noDataArr}) {
+export default function ResultBarChart({ xlabels, categoryWiseResult }) {
+  const [series, setSeries] = useState([]);
+  useEffect(() => {
+    setSeries((prev) => {
+      const yesData = xlabels.map((category) =>
+        categoryWiseResult[category] ? categoryWiseResult[category].Yes : 0
+      );
+      const noData = xlabels.map((category) =>
+        categoryWiseResult[category] ? categoryWiseResult[category].No : 0
+      );
+      const notApplicableData = xlabels.map((category) =>
+        categoryWiseResult[category]
+          ? categoryWiseResult[category]["Not Applicable"]
+          : 0
+      );
+      return [
+        { data: yesData, label: "Yes" },
+        { data: noData, label: "No" },
+        { data: notApplicableData, label: "Not Applicable" },
+      ];
+    });
+  }, [categoryWiseResult]);
+  console.log(series);
   return (
     <BarChart
-      width={800}
-      height={400}
-      series={[
-        { data: yesDataArr, label: 'Yes', id: 'yesId', stack: 'total' },
-        { data: noDataArr, label: 'No', id: 'noId', stack: 'total' },
+      xAxis={[
+        {
+          scaleType: "band",
+          data: xlabels,
+          label: "Categories of Green Practices",
+        },
       ]}
-      xAxis={[{ data: xlabels, scaleType: 'band' }]}
+      yAxis={[
+        {
+          label: "No of Practices",
+        },
+      ]}
+      sx={{
+        [`.${axisClasses.left} .${axisClasses.label}`]: {
+          transform: "translate(-20px, 0)",
+        },
+      }}
+      series={series}
+      width={1000}
+      height={500}
     />
   );
 }
--- frontend/src/pages/Analysis/Analysis.js
+++ frontend/src/pages/Analysis/Analysis.js
@@ -9,11 +9,13 @@ import ProjectType from "./../../api_results/projectType.json";
 
 const Analysis = () => {
   const [progressValue, setProgressValue] = useState(0);
-  const [totalQuestions, setTotalQuestions] = useState(graphResponse["totalQuestions"]);
+  const [totalQuestions, setTotalQuestions] = useState(
+    graphResponse["totalQuestions"]
+  );
   const [projectType, setProjectType] = useState(ProjectType["response"]);
-  const [isFormatted, setIsFormatted] = useState(false);
 
   const [categories, setCategories] = useState([]);
+
   const [yesDataArr, setYesDataArr] = useState([]);
   const [noDataArr, setNoDataArr] = useState([]);
   const [notApplicableDataArr, setNotApplicableDataArr] = useState([]);
@@ -21,91 +23,97 @@ const Analysis = () => {
   const [filteredResponse, setFilteredResponse] = useState([]);
   const [apiResponse, setApiResponse] = useState([]);
 
-  useEffect(() => {
-    if (graphResponse) {
-      const filtered = graphResponse["response"]?.filter(item => item.type !== "AI");
-      setFilteredResponse(filtered);
+  const [categoryWiseResult,setCategoryWiseResult]=useState({})
 
+  //to mock initialising and formatting backend data
+  useEffect(() => {
+    // filter the api response if needed
+    const updatedResponse = graphResponse["response"].filter((item) => {
+      return item.type && item.type != "AI";
+    });
+
+    setFilteredResponse(updatedResponse);
+
+    setCategories((prev) => {
       const uniqueCategories = new Set();
-      filtered?.forEach(item => {
+      updatedResponse.forEach((item) => {
         uniqueCategories.add(item.category);
       });
-      setCategories(Array.from(uniqueCategories));
-      setIsFormatted(true)
-    }
-  }, [graphResponse]);
+      return Array.from(uniqueCategories);
+    });
+  }, []);
 
- //to mock streaming affect
- useEffect(() => {
-  console.log("called mock service",apiResponse)
-  const interval = setInterval(() => {
-    if (isFormatted&& filteredResponse?.length > 0) {
-      // Get a random index
-      const randomIndex = Math.floor(Math.random() * filteredResponse.length);
-      // Get the random item
-      const randomItem = filteredResponse[randomIndex];
-
-      // Add the random item to apiResponse
-      setApiResponse((prevApiResponse) => [...prevApiResponse, randomItem]);
-
-      // Remove the random item from filteredResponse
-      setFilteredResponse((prevFilteredResponse) =>
-        prevFilteredResponse.filter((_, index) => index !== randomIndex)
-      );
-    }
-    return () => clearInterval(interval); // Cleanup interval on component unmount
-  }, 1000); // Runs every second
-
-  return () => clearInterval(interval);
-}, [isFormatted]);
-
-
+  //to mock streaming affect
   useEffect(() => {
-    changeProgressBar(apiResponse);
-    // changeBarChart(apiResponse);
-  }, 
-  // [apiResponse, categories]
-  [apiResponse]
+    const interval = setInterval(() => {
+      if (filteredResponse.length > 0) {
+        // Get a random index
+        const randomIndex = Math.floor(Math.random() * filteredResponse.length);
+        // Get the random item
+        const randomItem = filteredResponse[randomIndex];
 
-);
+        // Add the random item to apiResponse
+        setApiResponse((prevApiResponse) => [...prevApiResponse, randomItem]);
+
+        // Remove the random item from filteredResponse
+        setFilteredResponse((prevFilteredResponse) =>
+          prevFilteredResponse.filter((_, index) => index !== randomIndex)
+        );
+      }
+      return () => clearInterval(interval); // Cleanup interval on component unmount
+    }, 1000); // Runs every second
+
+    return () => clearInterval(interval);
+  }, [filteredResponse]);
+
+  // changes whenever data is streamed from backend
+  useEffect(() => {
+    //change progress bar
+    changeProgressBar(apiResponse);
+
+    //change bar chart
+    changeBarChart(apiResponse);
+  }, [apiResponse]);
 
   const changeProgressBar = (responseArr) => {
-    const length = responseArr?.length;
+    const length = responseArr.length;
     const value = Math.round((length / totalQuestions) * 100);
     setProgressValue(value);
   };
 
   const changeBarChart = (responseArr) => {
+   setCategoryWiseResult(prev=>{
     const resultCount = {};
 
-    categories.forEach(category => {
+    categories.forEach((category) => {
       resultCount[category] = { Yes: 0, No: 0, "Not Applicable": 0 };
     });
 
-    responseArr?.forEach(item => {
+    responseArr?.forEach((item) => {
       const { category, result } = item;
       if (resultCount[category]) {
         resultCount[category][result] += 1;
       }
     });
+    return resultCount
 
-    const yesData = categories.map(category => resultCount[category].Yes);
-    const noData = categories.map(category => resultCount[category].No);
-    const notApplicableData = categories.map(category => resultCount[category]["Not Applicable"]);
+   })
+    // const yesData = categories.map((category) => resultCount[category].Yes);
+    // const noData = categories.map((category) => resultCount[category].No);
+    // const notApplicableData = categories.map(
+    //   (category) => resultCount[category]["Not Applicable"]
+    // );
 
-    setYesDataArr(yesData);
-    setNoDataArr(noData);
-    setNotApplicableDataArr(notApplicableData);
+    // setYesDataArr(yesData);
+    // setNoDataArr(noData);
+    // setNotApplicableDataArr(notApplicableData);
   };
-
   return (
     <>
-      {/* <ResultBarChart
+      <ResultBarChart
         xlabels={categories}
-        yesDataArr={yesDataArr}
-        noDataArr={noDataArr}
-        notApplicableDataArr={notApplicableDataArr}
-      /> */}
+        categoryWiseResult={categoryWiseResult}
+      />
       <ResultPieChart />
       <ResultTabs />
       <ProgressTimer value={progressValue} />
