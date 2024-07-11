08516d7532d536c1f789dcfb15990533a010f606
Thuva <thuva.siva29@gmail.com>
2024-07-11 18:14:20 +0100

    ground truth complete for uber and dropbox

--- Rag/evaluation/metric_format.py
+++ Rag/evaluation/metric_format.py
@@ -1,8 +1,8 @@
 import json
 import pandas as pd
 import os
-GROUND_TRUTH_PATH="documentsFromText/Netflix/ground_truth.json"
-COMBINED_RESULT_PATH="Rag/logger/phi3_P2_Netflix_combined.csv"
+GROUND_TRUTH_PATH="documentsFromText/Uber/ground_truth.json"
+COMBINED_RESULT_PATH="Rag/logger/phi3_P2_Uber_combined.csv"
 EVAL_PATH="frontend/src/api_results/evaluation/results.json"
 def combine_groundTruth_result(ground_truth_path,combined_result_path):
 
